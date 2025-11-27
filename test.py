f1af99792d75686a123ba41c420548d4b3ed8837
rizqikapratamaa <rizqikapratamaa@gmail.com>
2025-11-27 07:26:56 +0700

    feat: clear folder and format

--- test.py
+++
@@ -1,6 +0,0 @@
-from sts.text_similarity import compute_semantic_similarity
-
-query = "Halo Kak, kami dari admin grup 'Info Crypto Valid'. Anda harus bayar biaya langganan bulanan 200rb untuk tetap dapat info coin yang bakal pump."
-
-results = compute_semantic_similarity(query, top_k=10)
-print(results)
